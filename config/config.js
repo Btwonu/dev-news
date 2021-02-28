c884db1a91371dc43b233e83837e82b52852191c
Val Delchev <btwonu@gmail.com>
2021-02-28 20:17:12 +0200

    Add config

---
+++ config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  smashing: {
+    baseURL: 'https://www.smashingmagazine.com',
+    articleURL: 'https://www.smashingmagazine.com/articles/',
+  },
+  hackernoon: {
+    baseURL: 'https://hackernoon.com/',
+    articleURL: 'https://hackernoon.com/tagged/hackernoon-top-story',
+  },
+  hackernews: {
+    baseURL: 'https://news.ycombinator.com/',
+    bestArticlesURL: 'https://hacker-news.firebaseio.com/v0/beststories.json',
+    articleURL: 'https://hacker-news.firebaseio.com/v0/item',
+  },
+  freecodecamp: {
+    baseURL: 'https://www.freecodecamp.org',
+    articleURL: 'https://www.freecodecamp.org/news',
+  },
+};
