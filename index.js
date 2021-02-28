c3dba3b10c15ef74f1f8dcdac50b0557d6800610
Val Delchev <btwonu@gmail.com>
2021-02-28 20:17:05 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+const {
+  getSmashing,
+  getHackernoon,
+  getHackernews,
+  getHackernewsById,
+  getLimited,
+} = require('./services/articleService');
+
+app.get('/smashing/:pageId?', (req, res) => {
+  const pageId = req.params.pageId;
+
+  getSmashing(pageId)
+    .then((articles) => {
+      res.json(articles);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/hackernoon', (req, res) => {
+  getHackernoon()
+    .then((articles) => {
+      res.json(articles);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/hackernews/:pageId?', (req, res) => {
+  const pageId = req.params.pageId;
+
+  getLimited(pageId)
+    .then((stories) => res.json(stories))
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
